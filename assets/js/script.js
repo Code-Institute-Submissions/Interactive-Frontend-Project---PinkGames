0a792fd3b0325c821cb5e66d2fb30721764371a9
emiberndt <emi.berndt@gmail.com>
2021-02-04 12:43:35 +0000

    Added background music

---
+++ assets/js/script.js
@@ -0,0 +1,13 @@
+// Background music
+let bgMusic = document.getElementById("bgMusic");
+let icon = document.getElementById("icon");
+
+icon.onclick = function(){
+    if(bgMusic.paused){
+        bgMusic.play();
+        icon.src = "assets/media/music/pause.png";
+    }else{
+        bgMusic.pause();
+        icon.src = "assets/media/music/play.png";
+    }
+}
