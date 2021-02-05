0312e0fabf3c138362f81927a0c69b8f3a33d95d
emiberndt <emi.berndt@gmail.com>
2021-02-05 15:36:05 +0000

    Functon to render the game

--- assets/js/pinkpong.js
+++ assets/js/pinkpong.js
@@ -60,8 +60,41 @@ function drawNet(){
     }
 }
 
+//The ball
+const ball = {
+    x: canvas.width/2,
+    y: canvas.height/2,
+    radius : 10,
+    velocityX : 5,
+    velocityY : 5,
+    speed : 5,
+    color : "#FF00AB"
+}
+
+//Render the game
+function render() {
+//Draw the canvas (court)
+drawRect(0, 0, canvas.width, canvas.height, "#FC8BD7");    
+// Draw the players score
+drawText(user.score, canvas.width/4, canvas.height/5, "#FF00AB");
+//Draw the computers score
+drawText(com.score, 3*canvas.width/4, canvas.height/5, "#FF00AB");
+//Draw the net
+drawNet();
+//Draw players paddle
 drawRect(user.x, user.y, user.width, user.height, user.color);
+//Draw computers paddle
 drawRect(com.x, com.y, com.width, com.height, com.color);
+// Draw the ball
+drawCircle(ball.x, ball.y, ball.radius, ball.color);
 
-
-
+}
+//Call the render
+function game() {
+    update();
+    render();
+}
+//50 frames per second
+const framesPerSecond = 50;
+//Call game 50 times every second
+setInterval(game, 1000/framesPerSecond);
