4823f5c9e922a31b71a7bdb921d3f1d49c15b660
emiberndt <emi.berndt@gmail.com>
2021-02-06 16:31:50 +0000

    In process of making the computer player's symbol select and show up on the game board.

--- assets/js/pinktactoe.js
+++ assets/js/pinktactoe.js
@@ -32,14 +32,48 @@ window.onload = () => {
 let xIcon = "fas fa-times";
 let oIcon = "far fa-circle";
 
+//The player click function
 function clickedBox(element) {
-
+    console.log(element);
     if (players.classList.contains("player")) {
         //Adding the 'circle-icon' to the O-player
         element.innerHTML = `<i class="${oIcon}"></i>`;
+        players.classList.add("active");
         //Adding the 'times-icon' to the X-player
     } else {
         element.innerHTML = `<i class="${xIcon}"></i>`;
+        players.classList.add("active");
     }
-
+    //When one 'span' has been selected, it can't be selected again
+    element.style.pointerEvents = "none";
+    com();
 };
+
+//The computer click function
+function com() {
+    //Unselected box index in this empty array
+    let array = [];
+    for (let i = 0; i < allBox.length; i++) {
+        //If span doesn't have a child element
+        if (allBox[i].childElementCount == 0) {
+            //Which span that has no children
+            array.push(i);
+            //console.log(i + " " + "has no child");
+        }
+    }
+    //Makes computer player select a random span (with no child) to play
+    let randomBox = array[Math.floor(Math.random() * array.length)];
+    console.log(randomBox);
+    if (array.lenght > 0) {
+        if (players.classList.contains("player")) {
+            //Adding the 'times-icon' to the X-player
+            allBox[randomBox].innerHTML = `<i class="${xIcon}"></i>`;
+            players.classList.add("active");
+            //Adding the 'circle-icon' to the O-player
+        } else {
+            allBox[randomBox].innerHTML = `<i class="${oIcon}"></i>`;
+            players.classList.add("active");
+        }
+    }
+    //console.log(array);
+}
