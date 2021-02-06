e96638ce69e5b5a17e2cc216f5158f788ec36323
emiberndt <emi.berndt@gmail.com>
2021-02-06 19:09:54 +0000

    Make game stop when someone has won

--- assets/js/pinktactoe.js
+++ assets/js/pinktactoe.js
@@ -69,44 +69,44 @@ function clickedBox(element) {
 //The computer player click function
 function com(runCom) {
     //If runcom is tru, then the code can be run
-    if(runCom) {
-            //If player has the X id, the computer will have the O id
-    playerSign = "O";
-    //Unselcted box index inside this empty array
-    let array = [];
-    for (let i = 0; i < allBox.length; i++) {
-        //If span has no children
-        if (allBox[i].childElementCount == 0) {
-            //The span has no children
-            array.push(i);
-            //console.log(i);
+    if (runCom) {
+        //If player has the X id, the computer will have the O id
+        playerSign = "O";
+        //Unselcted box index inside this empty array
+        let array = [];
+        for (let i = 0; i < allBox.length; i++) {
+            //If span has no children
+            if (allBox[i].childElementCount == 0) {
+                //The span has no children
+                array.push(i);
+                //console.log(i);
+            }
         }
-    }
-    //Random index from the array that the computer player will use to select a span
-    let randomBox = array[Math.floor(Math.random() * array.length)];
-    if (array.length > 0) {
-        if (players.classList.contains("player")) {
-            //Adding the 'times-icon' to the X-player
-            allBox[randomBox].innerHTML = `<i class="${xIcon}"></i>`;
-            players.classList.remove("active");
-            //If the player has the O id, the comuter will have the X id
-            playerSign = "X";
-            allBox[randomBox].setAttribute("id", playerSign);
-            //Adding the 'circle-icon' to the O-player
-        } else {
-            allBox[randomBox].innerHTML = `<i class="${oIcon}"></i>`;
-            players.classList.remove("active");
-            allBox[randomBox].setAttribute("id", playerSign);
+        //Random index from the array that the computer player will use to select a span
+        let randomBox = array[Math.floor(Math.random() * array.length)];
+        if (array.length > 0) {
+            if (players.classList.contains("player")) {
+                //Adding the 'times-icon' to the X-player
+                allBox[randomBox].innerHTML = `<i class="${xIcon}"></i>`;
+                players.classList.remove("active");
+                //If the player has the O id, the comuter will have the X id
+                playerSign = "X";
+                allBox[randomBox].setAttribute("id", playerSign);
+                //Adding the 'circle-icon' to the O-player
+            } else {
+                allBox[randomBox].innerHTML = `<i class="${oIcon}"></i>`;
+                players.classList.remove("active");
+                allBox[randomBox].setAttribute("id", playerSign);
+            }
+            //Calling the winner of the game
+            selectWinner();
         }
-        //Calling the winner of the game
-        selectWinner();
+        //After a box has been played it can't be played again
+        allBox[randomBox].style.pointerEvents = "none";
+        gameBoard.style.pointerEvents = "auto";
+        playerSign = "X";
     }
-    //After a box has been played it can't be played again
-    allBox[randomBox].style.pointerEvents = "none";
-    gameBoard.style.pointerEvents = "auto";
-    playerSign = "X";
 }
-    }
 
 //Selectiong the winner
 function getId(idname) {
@@ -121,15 +121,17 @@ function checkId(val1, val2, val3, sign) {
 }
 function selectWinner() {
     //One of these combination will make a winner
-    if (checkId(1, 2, 3, playerSign) || 
-    checkId(4, 5, 6, playerSign) || 
-    checkId(7, 8, 9, playerSign) || 
-    checkId(1, 4, 7, playerSign) || 
-    checkId(2, 5, 8, playerSign) || 
-    checkId(3, 6, 9, playerSign) || 
-    checkId(1, 5, 9, playerSign) || 
-    checkId(3, 5, 7, playerSign)) {
+    if (checkId(1, 2, 3, playerSign) ||
+        checkId(4, 5, 6, playerSign) ||
+        checkId(7, 8, 9, playerSign) ||
+        checkId(1, 4, 7, playerSign) ||
+        checkId(2, 5, 8, playerSign) ||
+        checkId(3, 6, 9, playerSign) ||
+        checkId(1, 5, 9, playerSign) ||
+        checkId(3, 5, 7, playerSign)) {
         console.log(playerSign + " " + "is the winner");
-
+        //When someone has won the game the runcom will stop running
+        runCom = false;
+        com(runCom);
     }
 }
