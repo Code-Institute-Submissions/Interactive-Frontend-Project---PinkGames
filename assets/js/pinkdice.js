e1864bc0a9ff74399ee6f5e931e8f99439a5071f
emiberndt <emi.berndt@gmail.com>
2021-02-07 13:55:23 +0000

    Cleaned uo code, had missed a few semi-colons

--- assets/js/pinkdice.js
+++ assets/js/pinkdice.js
@@ -1,10 +1,10 @@
 function playDice() {
     //Get random number
     //Get the dice-image to go with the random number
-    let randomNumberOne = Math.floor(Math.random() * 6) + 1 //Beacuse the dice starts at 1
+    let randomNumberOne = Math.floor(Math.random() * 6) + 1; //Beacuse the dice starts at 1
     let randomImageOnePath = "assets/media/pic/pinkdice/dices/" + randomNumberOne + ".png";
 
-    let randomNumberTwo = Math.floor(Math.random() * 6) + 1
+    let randomNumberTwo = Math.floor(Math.random() * 6) + 1;
     let randomImageTwoPath = "assets/media/pic/pinkdice/dices/" + randomNumberTwo + ".png";
 
     let image1 = document.querySelectorAll("img")[0];
@@ -15,11 +15,11 @@ function playDice() {
 
     //Who wins the game
     if (randomNumberOne > randomNumberTwo) {
-        document.querySelector("h1").innerHTML = "Player 1 wins!"
+        document.querySelector("h1").innerHTML = "Player 1 wins!";
     } else if (randomNumberTwo > randomNumberOne) {
-        document.querySelector("h1").innerHTML = "Player 2 wins!"
+        document.querySelector("h1").innerHTML = "Player 2 wins!";
     } else {
-        document.querySelector("h1").innerHTML = "It's a draw!"
+        document.querySelector("h1").innerHTML = "It's a draw!";
     }
 }
 
